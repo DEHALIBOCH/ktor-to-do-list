28ca8c172f9bffe63d18ee94e06a5b49592aaca8
Marat <115820948+DEHALIBOCH@users.noreply.github.com>
2023-10-01 20:38:58 +0600

    CRUD
    * Code style
    
    * Entity for receiving data from client
    
    * List to MutableList for CRUD operations
    
    * CRUD methods
    
    * CRUD methods implementation
    
    * Implementation CRUD methods routing

---
+++ src/main/kotlin/kz/dehaliboch/entities/ToDoDraft.kt
@@ -0,0 +1,6 @@
+package kz.dehaliboch.entities
+
+data class ToDoDraft(
+    val title: String,
+    val isDone: Boolean,
+)
--- src/main/kotlin/kz/dehaliboch/repository/ToDoRepository.kt
+++ src/main/kotlin/kz/dehaliboch/repository/ToDoRepository.kt
@@ -1,10 +1,17 @@
 package kz.dehaliboch.repository
 
 import kz.dehaliboch.entities.ToDo
+import kz.dehaliboch.entities.ToDoDraft
 
 interface ToDoRepository {
 
-    fun getAllToDo() : List<ToDo>
+    fun getAllToDo(): List<ToDo>
 
-    fun getToDoById(id: Int) : ToDo?
+    fun getToDoById(id: Int): ToDo?
+
+    fun addToDo(toDoDraft: ToDoDraft): ToDo
+
+    fun removeToDo(id: Int): Boolean
+
+    fun update(id: Int, toDoDraft: ToDoDraft): Boolean
 }
