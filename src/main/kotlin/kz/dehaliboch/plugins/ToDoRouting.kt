11f9d80f4a0acd8cc3931fafd5eb56b09ddbdb2b
marat <almabekov.marat@gmail.com>
2023-10-01 20:37:06 +0600

    Implementation CRUD methods routing

--- src/main/kotlin/kz/dehaliboch/plugins/ToDoRouting.kt
+++ src/main/kotlin/kz/dehaliboch/plugins/ToDoRouting.kt
@@ -2,11 +2,13 @@ package kz.dehaliboch.plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kz.dehaliboch.comon.ID
 import kz.dehaliboch.comon.TO_DO_PATH
 import kz.dehaliboch.comon.TO_DO_PATH_BY_ID
+import kz.dehaliboch.entities.ToDoDraft
 import kz.dehaliboch.repository.InMemoryToDoRepository
 import kz.dehaliboch.repository.ToDoRepository
 
@@ -44,15 +46,47 @@ fun Application.configureToDoRouting() {
         }
 
         post(TO_DO_PATH) {
+            val toDoDraft = call.receive<ToDoDraft>()
 
+            call.respond(toDoRepository.addToDo(toDoDraft))
         }
 
         put(TO_DO_PATH_BY_ID) {
+            val id = call.parameters[ID]?.toIntOrNull()
+            val toDoDraft = call.receive<ToDoDraft>()
 
+            if (id == null) {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    "id need to be a number"
+                )
+                return@put
+            }
+
+
+            if (toDoRepository.update(id, toDoDraft)) {
+                call.respond(HttpStatusCode.OK)
+            } else {
+                call.respond(HttpStatusCode.NotFound, "Found no To-Do for provided id #$id")
+            }
         }
 
         delete(TO_DO_PATH_BY_ID) {
+            val id = call.parameters[ID]?.toIntOrNull()
 
+            if (id == null) {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    "id need to be a number"
+                )
+                return@delete
+            }
+
+            if (toDoRepository.removeToDo(id)) {
+                call.respond(HttpStatusCode.OK)
+            } else {
+                call.respond(HttpStatusCode.NotFound, "Found no To-Do for provided id #$id")
+            }
         }
     }
 }
