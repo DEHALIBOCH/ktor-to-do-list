c47f7afc1b88da86fdaf6c7ddb0a6bd8f924e86d
marat <almabekov.marat@gmail.com>
2023-10-01 20:36:35 +0600

    CRUD methods implementation

--- src/main/kotlin/kz/dehaliboch/repository/InMemoryToDoRepository.kt
+++ src/main/kotlin/kz/dehaliboch/repository/InMemoryToDoRepository.kt
@@ -1,15 +1,40 @@
 package kz.dehaliboch.repository
 
-import kz.dehaliboch.comon.InMemoryDB
+import kz.dehaliboch.comon.InMemoryDB.todoList
 import kz.dehaliboch.entities.ToDo
+import kz.dehaliboch.entities.ToDoDraft
 
 class InMemoryToDoRepository : ToDoRepository {
 
     override fun getAllToDo(): List<ToDo> {
-        return InMemoryDB.todoList
+        return todoList
     }
 
     override fun getToDoById(id: Int): ToDo? {
-        return InMemoryDB.todoList.firstOrNull { toDo -> toDo.id == id }
+        return todoList.firstOrNull { toDo -> toDo.id == id }
+    }
+
+    override fun addToDo(toDoDraft: ToDoDraft): ToDo {
+        val todo = ToDo(
+            id = todoList.size + 1,
+            title = toDoDraft.title,
+            isDone = toDoDraft.isDone,
+        )
+        todoList.add(todo)
+
+        return todo
+    }
+
+    override fun removeToDo(id: Int): Boolean {
+        return todoList.removeIf { it.id == id }
+    }
+
+    override fun update(id: Int, toDoDraft: ToDoDraft): Boolean {
+        val todo = todoList.firstOrNull { it.id == id } ?: return false
+
+        todo.title = toDoDraft.title
+        todo.isDone = toDoDraft.isDone
+
+        return true
     }
 }
